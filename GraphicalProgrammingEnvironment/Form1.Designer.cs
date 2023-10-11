cf0f6bff5db3c199e2552fb4f62b5614747bfc4c
WaniAjayi <S.Ajayi9355@student.leedsbeckett.ac.uk>
2023-10-11 21:44:02 +0100

    User Interface

---
+++ GraphicalProgrammingEnvironment/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GraphicalProgrammingEnvironment
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(21, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(303, 259);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 277);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(303, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(21, 320);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 31);
+            button1.TabIndex = 2;
+            button1.Text = "RUN";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(127, 320);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 31);
+            button2.TabIndex = 3;
+            button2.Text = "SYNTAX";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Location = new Point(392, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(378, 280);
+            panel1.TabIndex = 4;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, 368);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(749, 136);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 506);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Graphical Programming Environment";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+    }
+}
